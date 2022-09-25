b251dec2a74e694de741857f5248086a62bd5d09
mubashir <imubasherahmad@gmail.com>
2022-09-25 13:22:36 +0500

    10-serving-files-statically

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const app = express()
 
 // body-parser
 app.use(express.urlencoded({extended: true}))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/admin', adminRoutes)
 app.use(shopRoutes)
