4a6e6b46fe760a989123774b053be47deb9077e3
mubashir <imubasherahmad@gmail.com>
2022-09-26 22:18:36 +0500

    14. getting started with handlebars

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const path = require('path')
 
 const express = require('express')
+const { engine } = require('express-handlebars')
 
 const adminData = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
 const app = express()
 
-app.set('view engine', 'pug')
+// file extension will be the name registered in engine
+app.engine('hbs', engine({
+  extname: 'hbs',
+  defaultLayout: 'main-layout',
+}))
+app.set('view engine', 'hbs')
 app.set('views', 'views')
 
 // body-parser
--- routes/shop.js
+++ routes/shop.js
@@ -9,7 +9,14 @@ const router = express.Router()
 
 router.get('/', (req, res, next) => {
   const {products} = adminData
-  res.render('shop', { products , docTitle: 'Shop', path: '/'})
+  res.render('shop', { 
+    products, 
+    docTitle: 'Shop', 
+    path: '/', 
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true
+  })
 })
 
 module.exports = router
