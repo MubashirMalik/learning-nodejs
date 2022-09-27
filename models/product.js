8bdc52910454fe81161200c99f9336a06f616032
mubashir <imubasherahmad@gmail.com>
2022-09-27 23:28:12 +0500

    21. writing & reading model to/from file

--- models/product.js
+++ models/product.js
@@ -1,4 +1,20 @@
-const products = []
+const fs = require('fs')
+const path = require('path')
+
+const filePath = path.join(
+  path.dirname(process.mainModule.filename), 
+  'data',
+  'products.json'
+)
+
+const getProductsFromFile = (callback) => {
+  fs.readFile(filePath, (err, fileContent) => {
+    if (err) {
+      return callback([])
+    }
+    return callback(JSON.parse(fileContent))
+  })
+}
 
 module.exports = class Product {
   constructor(title) {
@@ -6,10 +22,17 @@ module.exports = class Product {
   }
 
   save() {
-    products.push(this)
+    getProductsFromFile(products => {
+      products.push(this)
+      fs.writeFile(filePath, JSON.stringify(products), (err) => {
+        if (err) {
+          console.log(err)
+        }
+      })
+    })
   }
 
-  static fetchAll() {
-    return products
+  static fetchAll(callback) {
+    getProductsFromFile(callback)
   }
 }
